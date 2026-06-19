440701164b9239e1a7e696c86498b5481aca5f57
daemunozr <dae.munozr@duocuc.cl>
2026-06-18 23:41:28 -0400

    eliminado dto vacios

--- src/main/java/com/sivebo/ms_ventas/dto/VentaRequestDTO.java
+++
@@ -1,2 +0,0 @@
-package com.sivebo.ms_ventas.dto;
-// Replaced by dto/request/VentaRequestDTO.java — delete this file
--- src/main/java/com/sivebo/ms_ventas/dto/VentaResponseDTO.java
+++
@@ -1,2 +0,0 @@
-package com.sivebo.ms_ventas.dto;
-// Replaced by dto/response/VentaResponseDTO.java — delete this file
